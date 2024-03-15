a2bd5bc485febc057afb16b3db7e1b08f9e04d7c
Aman88600 <109413130+Aman88600@users.noreply.github.com>
2024-03-15 12:22:53 +0530

    rock_paper_scissiors

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -2,11 +2,37 @@
 
 import random
 computer_choice = random.randint(1,3)
-
+user_input = input(f"Enter your choice 1 rock 2 paper 3 Scissors : ")
 #printing computer choice
-if computer_choice == 1:
-    print("Rock")
-elif computer_choice == 2:
-    print("Paper")
-elif computer_choice == 3:
-    print("Scissors")
+def choice(choice):
+    if choice == 1:
+        print("""
+        _______
+    ---'   ____)
+        (_____)
+        (_____)
+        (____)
+    ---.__(___)
+    """)
+    elif choice == 2:
+        # Paper
+        print("""
+        _______
+    ---'    ____)____
+            ______)
+            _______)
+            _______)
+    ---.__________)
+    """)
+    elif choice == 3:
+        # Scissors
+        print("""
+        _______
+    ---'   ____)____
+            ______)
+        __________)
+        (____)
+    ---.__(___)
+    """)
+        
+print(f"Your choice {choice(user_input)}\n\n\n computer choice = {choice(computer_choice)}")
