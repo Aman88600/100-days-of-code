f39444a0e01ae79d939ecb6f193e06d527dfa8f0
Aman88600 <109413130+Aman88600@users.noreply.github.com>
2024-03-15 12:14:46 +0530

    rock_paper_scissiors

---
+++ Rock_Paper_Scissors.py
@@ -0,0 +1,12 @@
+# Making a rock, paper, Scissors game in 
+
+import random
+computer_choice = random.randint(1,3)
+
+#printing computer choice
+if computer_choice == 1:
+    print("Rock")
+elif computer_choice == 2:
+    print("Paper")
+elif computer_choice == 3:
+    print("Scissors")
