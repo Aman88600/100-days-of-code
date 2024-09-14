544f954defd1042d9d761d75acd88ccd9da75c13
Aman88600 <amanbasoya02@gmail.com>
2024-09-14 18:51:03 +0530

    Coffee machine code is done for use :-)

--- Day-15/coffee_machine.py
+++ Day-15/coffee_machine.py
@@ -39,21 +39,44 @@ resources = {
     "money": 0,
 }
 
+# Function to check if we have enough resources to make coffee
 def check_resources(user_input):
-    # Condition to check espresso
     if user_input != "espresso":
         if (resources["milk"] < MENU[user_input]["ingredients"]["milk"]):
-            return "Sorry there is not enough milk"
+            return ["Sorry there is not enough milk", False]
     if (resources["water"] < MENU[user_input]["ingredients"]["water"]):
-        return "Sorry ther is not enough water"
+        return ["Sorry ther is not enough water", False]
     elif (resources["coffee"] < MENU[user_input]["ingredients"]["coffee"]):
-        return "Sorry there is not enough cofee"
+        return ["Sorry there is not enough cofee", False]
     else:
-        resources["water"] -= MENU[user_input]["ingredients"]["water"]
-        if user_input != "espresso":
-            resources["milk"] -= MENU[user_input]["ingredients"]["milk"]
-        resources["coffee"] -= MENU[user_input]["ingredients"]["coffee"]
-        return f"Here is your {user_input} {coffee_emoji} Enjoy!"
+        return [f"Here is your {user_input} {coffee_emoji} Enjoy!", True]        
+    
+
+# Insert coin function
+def insert_coin(user_input):
+    print("Please insert coins.")
+    quarter = float(input("How many quarters?: "))
+    dime = float(input("How many dimes?: "))
+    nickel = float(input("How many nickles?: "))
+    penny = float(input("How many pennies?: "))
+    total_value = (COIN_VALUES["quarter"] * quarter) + (COIN_VALUES["dime"] * dime) + (COIN_VALUES["nickel"] * nickel) + (COIN_VALUES["penny"] * penny) 
+    if MENU[user_input]["cost"] <= total_value:
+        # Adding money to machine
+        resources["money"] += total_value
+        # Giving user change back
+        amount = total_value - MENU[user_input]["cost"] 
+        formatted_price = "{:.2f}".format(amount)
+        print(f"Here is ${formatted_price} in change") 
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+
+def make_coffee(user_input):
+    resources["water"] -= MENU[user_input]["ingredients"]["water"]
+    if user_input != "espresso":
+        resources["milk"] -= MENU[user_input]["ingredients"]["milk"]
+    resources["coffee"] -= MENU[user_input]["ingredients"]["coffee"]
+    return f"Here is your {user_input} {coffee_emoji} Enjoy!"
 
 
 # The coffee machine will run until stopped
@@ -70,11 +93,27 @@ while True:
         print(f"Money: ${resources["money"]}")
     # latte feature
     elif user_input == "latte":
-        print(check_resources(user_input))
+        function_output = check_resources(user_input)
+        if function_output[1]:
+            if insert_coin(user_input):
+                print(make_coffee(user_input))
+        else:
+            print(function_output[0])
     # espresso feature
     elif user_input == "espresso":
-        print(check_resources(user_input))
+        function_output = check_resources(user_input)
+        if function_output[1]:
+            if insert_coin(user_input):
+                print(make_coffee(user_input))
+        else:
+            print(function_output[0])
+    # cappuccino feature
     elif user_input == "cappuccino":
-        print(check_resources(user_input))
+        function_output = check_resources(user_input)
+        if function_output[1]:
+            if insert_coin(user_input):
+                print(make_coffee(user_input))
+        else:
+            print(function_output[0])
     else:
         print(f"{user_input} is a invalid Input")
