b47dcef53d2f91acf02346a08e15e6fba68fd8ba
Aman88600 <amanbasoya02@gmail.com>
2024-09-15 11:03:13 +0530

    Object table creating challenge is complete

--- Day-16/turtule_module_use.py
+++ Day-16/turtule_module_use.py
@@ -1,15 +1,18 @@
-from turtle import Turtle, Screen
-# from another_module import another_variable
-# print(another_variable)
+# from turtle import Turtle, Screen
+# # from another_module import another_variable
+# # print(another_variable)
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("coral")
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("coral")
 
-my_screen = Screen()
-print(my_screen.canvheight)
-timmy.forward(100)
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# timmy.forward(100)
 
-# This function will hold the screen until we click in it
-my_screen.exitonclick()
+# # This function will hold the screen until we click in it
+# my_screen.exitonclick()
+
+from prettytable import PrettyTabel
+table = PrettyTabel()
