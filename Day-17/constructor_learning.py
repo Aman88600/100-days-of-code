a37e644ee9f362f7a5399083af6885aab24a5761
Aman88600 <amanbasoya02@gmail.com>
2024-09-17 13:26:44 +0530

    Understanding the variables/methods of the class

--- Day-17/constructor_learning.py
+++ Day-17/constructor_learning.py
@@ -1,5 +1,7 @@
 class User:
-    def __init__(self):
-        print("New user being created!")
-
-user_1 = User()
+    def __init__(self, username):
+        self.username = username
+        # Default attribute
+        self.followers = 0
+user_1 = User("Aman")
+print(user_1.username, user_1.followers)
