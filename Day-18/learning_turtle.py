946769a5f1409e27ed518425dbc34f6131a42fc7
Aman88600 <amanbasoya02@gmail.com>
2024-09-21 20:20:42 +0530

    turtle basics are done

--- Day-18/learning_turtle.py
+++ Day-18/learning_turtle.py
@@ -1,5 +1,10 @@
 from turtle import Turtle, Screen
 
 timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("red")
+for i in range(10):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(180)
 screen = Screen()
 screen.exitonclick()
