ec0de38479d4a54cc0457a8df5fb68d4426b918e
Aman88600 <amanbasoya02@gmail.com>
2024-09-26 16:38:15 +0530

    onkey() is a higher order function because it can take other functions as input]

---
+++ learning_1.py
@@ -0,0 +1,12 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+window = Screen()
+
+def move_forward():
+    tim.forward(10)
+
+window.listen()
+window.onkey(key="space", fun=move_forward) # Higher order function
+
+window.exitonclick()
