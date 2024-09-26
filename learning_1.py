c2ea6bce491e0a3dbf19942180ae9b7e51a0822e
Aman88600 <amanbasoya02@gmail.com>
2024-09-26 16:42:56 +0530

    some simple modifications

--- learning_1.py
+++ learning_1.py
@@ -6,7 +6,10 @@ window = Screen()
 def move_forward():
     tim.forward(10)
 
-window.listen()
-window.onkey(key="space", fun=move_forward) # Higher order function
+def hello():
+    print("Hello, World!")
 
+window.listen()
+window.onkey(key="space", fun=move_forward) # onkey is a higher order function, reason is because it can take other function as input
+window.onkey(key="w", fun=hello)
 window.exitonclick()
