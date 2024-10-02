04d772c03d84fa5674f7dae7982c9c5cfae03c29
Aman88600 <amanbasoya02@gmail.com>
2024-10-02 13:10:57 +0530

    Did some stuff with the snake game

--- Day-20/snake_game.py
+++ Day-20/snake_game.py
@@ -2,5 +2,11 @@ from turtle import Turtle, Screen
 
 
 snake = Turtle()
+snake.color("white")
+snake.shape("square")
+
 window = Screen()
+window.title("Snake Game")
+window.bgcolor("Black")
+window.setup(width = 600, height = 500)
 window.exitonclick()
