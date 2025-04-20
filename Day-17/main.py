ae62803bc73302401b7422e8c7285cb6c9076907
Aman88600 <amanbasoya02@gmail.com>
2025-04-20 14:32:38 +0530

    'new_start'

--- Day-17/main.py
+++
@@ -1,6 +0,0 @@
-from question_model import Question
-from data import question_data
-
-question_bank = []
-for element in question_data:
-    question_bank.append(Question(text = element['text'], answer = element['answer']))
