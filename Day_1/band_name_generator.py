46af6f7d4605eaf07b007e4c4c5965c354429cc7
Aman88600 <amanbasoya02@gmail.com>
2025-04-21 23:32:46 +0530

    Day_1

---
+++ Day_1/band_name_generator.py
@@ -0,0 +1,11 @@
+# This is the solution for Day 1, I am doing this a little early cause, I have to sleep soon and I want to make a commit
+
+# 1. We Greet the user
+print("Welcome to the Band Name Generator (Made by Aman Basoya).")
+# 2. We take the name of the city using the input() function
+city = input("What's the name of the city you grew up in? : ")
+# 3. We Ask for the user's pet's name
+pet_name = input("What's Your pet's name : ")
+# 4. Finally Printing the Band name
+band_name = f"{city} {pet_name}"
+print(f"Your Band Name could be {band_name}")
