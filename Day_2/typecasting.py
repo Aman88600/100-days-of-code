734ac418023ce9293aadfeca8b1fe82e232b8329
Aman88600 <amanbasoya02@gmail.com>
2025-05-17 12:43:22 +0530

    typecasting

---
+++ Day_2/typecasting.py
@@ -0,0 +1,3 @@
+# print("Number of Letters in your name : " + len(input("Enter Your Name : ")))
+# int() str() bool() float()
+print("Number of Letters in your name : " + str(len(input("Enter Your Name : "))))
