a09ecdd8f9598e47b3e2985bd2f3c78635ad6fef
Aman88600 <amanbasoya02@gmail.com>
2025-05-18 15:59:56 +0530

    'd'

--- Day_3/ticket_counter_2.py
+++ Day_3/ticket_counter_2.py
@@ -1,14 +1,19 @@
 height = int(input('Enter Your Height in cm : '))
+# This is the total bill
+bill = 0
 if height > 120:
     age = int(input('Enter Your Age : '))
     want_photo = input('Do you want a photo of you at roller coaster y/n:')
     if age < 12:
-        print('That will be $5')
+        bill += 5
     elif age >= 12 and age <= 18:
-        print('That will be $7')
+        bill += 7
+    elif age >= 45 and age <= 55:
+        bill += 0
     else:
-        print('That\'ll be $12')
+        bill += 12
     if want_photo == 'y':
-        print('That will be extra $3 for photo')
+        bill += 3
+    print(f'Your Bill is {bill}')
 else:
     print('Can\'t ride')
