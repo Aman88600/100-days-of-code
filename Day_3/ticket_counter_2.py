edbfde0538c9e952ce0d058985e11254e84749de
Aman88600 <amanbasoya02@gmail.com>
2025-05-18 14:38:00 +0530

    'tic'

--- Day_3/ticket_counter_2.py
+++ Day_3/ticket_counter_2.py
@@ -1,9 +1,11 @@
 height = int(input('Enter Your Height in cm : '))
 if height > 120:
     age = int(input('Enter Your Age : '))
-    if age > 18:
-        print('That will be $12')
+    if age < 12:
+        print('That will be $5')
+    elif age >= 12 and age <= 18:
+        print('That will be $7')
     else:
-        print('That\'ll be $7')
+        print('That\'ll be $12')
 else:
     print('Can\'t ride')
