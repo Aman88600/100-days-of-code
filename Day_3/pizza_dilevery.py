7cbea4da7745d65bb13509c7a95c11c8084eccdd
Aman88600 <amanbasoya02@gmail.com>
2025-05-18 15:42:23 +0530

    'pizza'

---
+++ Day_3/pizza_dilevery.py
@@ -0,0 +1,29 @@
+print('Welcome to python Pizza Delevery!')
+size = input('What size of pizza do you want S, M or L : ')
+pepperoni = input('Do you want pepperoni on your pizza Y or N :')
+extra_cheese = input("Do you want extra cheese Y or N: ")
+
+bill = 0
+if size == 'S':
+    bill += 15
+    if pepperoni == 'Y':
+        bill += 2
+elif size == 'M':
+    bill += 20
+    if pepperoni == 'Y':
+        bill += 3
+elif size == 'L':
+    bill += 25
+    if pepperoni == 'Y':
+        bill += 3
+else:
+    print("Invalid Input in pizza size")
+    exit(0)
+
+if extra_cheese == 'Y':
+    bill += 1
+
+print(f'You total bill is ${bill}')
+
+
+
