7378c357f3139a579823ec3ce938e5c3e4534500
Aman88600 <amanbasoya02@gmail.com>
2025-05-19 12:45:19 +0530

    'Day_6'

---
+++ Day_6/maze_solution.py
@@ -0,0 +1,13 @@
+# This code solves any maze
+def turn_right():
+    for i in range(3):
+        turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
