e08dce69add5f36134bb904edcd04424b44d6b35
Aman88600 <amanbasoya02@gmail.com>
2025-06-06 16:13:03 +0530

    'car_is_running'

--- Day_23/turtle_crossing_game.py
+++ Day_23/turtle_crossing_game.py
@@ -7,17 +7,27 @@ width, height = 500, 500
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Turtle Crossing Game")
 
-car_x = 0
+car_x_y = [490, 100]
+def get_new_color():
+    return (randint(0, 255), randint(0, 255), randint(0, 255))
+color = get_new_color()
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    
-    pygame.draw.rect(window, (255, 255, 255), (car_x, 10, 10, 10))
-    car_x += 1
-    clock.tick(60)
+
+    # Clear the screen by filling with black
+    window.fill((0,0,0))
+    car_x_y[0] -= randint(1,10)
+    if car_x_y[0] < -30:
+        car_x_y[0] = 500
+        car_x_y[1] = randint(0,490)
+        color = get_new_color()
+    pygame.draw.rect(window, color, (car_x_y[0], car_x_y[1], 30, 10))
     pygame.display.flip()
+    clock.tick(120)
 # Quit Pygame
 pygame.quit()
