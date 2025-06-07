9c03726c7a347c33546b66d12338b1d22764aa49
Aman88600 <amanbasoya02@gmail.com>
2025-06-07 16:37:11 +0530

    'turtle_without_collision_detection'

--- Day_23/turtle_crossing_game.py
+++ Day_23/turtle_crossing_game.py
@@ -8,6 +8,8 @@ window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Turtle Crossing Game")
 
 
+# Turtle Crossing
+turtle_x_y = [250, 490]
 # Number of cars
 number_of_cars = 10
 car_x_y = []
@@ -26,7 +28,13 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                turtle_x_y[1] -= 10
 
+
+    if turtle_x_y[1] == 0:
+        break
     # Clear the screen by filling with black
     window.fill((0,0,0))
     for i in range(0,number_of_cars):
@@ -38,6 +46,9 @@ while running:
 
     for i in range(0, number_of_cars):
         pygame.draw.rect(window, color[i], (car_x_y[i][0], car_x_y[i][1], 30, 10))
+    
+    # Making the turtle
+    pygame.draw.rect(window,(255, 255, 255), (turtle_x_y[0], turtle_x_y[1], 10, 10))
     pygame.display.flip()
     clock.tick(120)
 # Quit Pygame
