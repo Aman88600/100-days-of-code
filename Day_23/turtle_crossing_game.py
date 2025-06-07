31420b8be35f5eb0a672847eb0d3fcc9d6c0da87
Aman88600 <amanbasoya02@gmail.com>
2025-06-07 11:56:17 +0530

    'cars_allmost_done'

--- Day_23/turtle_crossing_game.py
+++ Day_23/turtle_crossing_game.py
@@ -7,11 +7,19 @@ width, height = 500, 500
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Turtle Crossing Game")
 
-car_x_y = [490, 100]
+
+# Number of cars
+number_of_cars = 10
+car_x_y = []
+
+for i in range(0,number_of_cars):
+    car_x_y.append([490, randint(0, 490)])
+
 def get_new_color():
     return (randint(0, 255), randint(0, 255), randint(0, 255))
-color = get_new_color()
-
+color = []
+for i in range(0, number_of_cars):
+    color.append(get_new_color())
 running = True
 while running:
     for event in pygame.event.get():
@@ -21,12 +29,15 @@ while running:
 
     # Clear the screen by filling with black
     window.fill((0,0,0))
-    car_x_y[0] -= randint(1,10)
-    if car_x_y[0] < -30:
-        car_x_y[0] = 500
-        car_x_y[1] = randint(0,490)
-        color = get_new_color()
-    pygame.draw.rect(window, color, (car_x_y[0], car_x_y[1], 30, 10))
+    for i in range(0,number_of_cars):
+        car_x_y[i][0] -= randint(1,10)
+        if car_x_y[i][0] < -30:
+            car_x_y[i][0] = 500
+            car_x_y[i][1] = randint(0,490)
+            color[i] = get_new_color()
+
+    for i in range(0, number_of_cars):
+        pygame.draw.rect(window, color[i], (car_x_y[i][0], car_x_y[i][1], 30, 10))
     pygame.display.flip()
     clock.tick(120)
 # Quit Pygame
