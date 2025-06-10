1d37ed5caec87cac2c2aa46c0775e1c439dab9a3
Aman88600 <amanbasoya02@gmail.com>
2025-06-10 23:55:05 +0530

    'man_these_pandas_series_and_data_frames_are_cool_as_sh!t'

--- Day_25/main.py
+++ Day_25/main.py
@@ -4,15 +4,30 @@
 
 # print(data)
 
-import csv
+# import csv
 
-temperatures = []
-with open("weather_data.csv", "r") as file:
-    data = csv.reader(file)
-    # Now that we have the data we can now show it
-    for row in data:
-        if row[1] == 'temp':
-            pass
-        else:
-            temperatures.append(int(row[1]))
-print(temperatures)
+# temperatures = []
+# with open("weather_data.csv", "r") as file:
+#     data = csv.reader(file)
+#     # Now that we have the data we can now show it
+#     for row in data:
+#         if row[1] == 'temp':
+#             pass
+#         else:
+#             temperatures.append(int(row[1]))
+# print(temperatures)
+
+import pandas
+data  = pandas.read_csv("weather_data.csv")
+# print(data["temp"])
+print(data.to_dict())
+
+temp_list = data["temp"].to_list()
+
+# TO find the avg temp
+sum_temp = 0
+for temp in temp_list:
+    sum_temp += temp
+
+average_temp = sum_temp / len(temp_list)
+print(average_temp)
