5325d23dd73e6cf0580dc3817a83e72ed5d17362
Aman88600 <amanbasoya02@gmail.com>
2025-06-12 23:11:20 +0530

    'list_comphrension_with_condition'

--- Day_26/list_comprehension.py
+++ Day_26/list_comprehension.py
@@ -1,8 +1,12 @@
 # [new_item for item in list]
 
-numbers = [i for i in range(1,11)]
+# numbers = [i for i in range(1,11)]
 # new_numbers = [(number + 1) for number in numbers]
 # print(new_numbers)
 
-numbers_2 = [num*2 for num in numbers]
-print(numbers_2)
+# numbers_2 = [num*2 for num in numbers]
+# print(numbers_2)
+
+names = ["Aman", "Nikhil", "Mukunds"]
+short_names = [name for name in names if len(name) <= 4]
+print(short_names)
