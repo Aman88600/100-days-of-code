0b583d325d2cd4ca4dc183ff833e8e0052120e71
Aman88600 <amanbasoya02@gmail.com>
2025-06-15 10:22:30 +0530

    'entry'

--- Day_27/tk_button.py
+++ Day_27/tk_button.py
@@ -7,12 +7,18 @@ new_label = Label(text="0")
 new_label.pack()
 
 
+# Entry
+input = Entry(width=10)
+input.pack()
+
 
 # Button
 def button_click():
-    num = int(new_label["text"])
-    num += 1
-    new_label["text"] = num
+    # num = int(new_label["text"])
+    # num += 1
+    new_label["text"] = input.get()
 my_button = Button(text="This is a button", command=button_click)
 my_button.pack()
+
+
 window.mainloop()
