b247920479e4ce203cdf606b6168b2914d7d1462
Aman88600 <amanbasoya02@gmail.com>
2025-07-18 19:28:52 +0530

    'name_link_and_up_votes'

--- Day_45/bs4-start/main.py
+++ Day_45/bs4-start/main.py
@@ -7,7 +7,17 @@ response = requests.get(url="https://appbrewery.github.io/news.ycombinator.com/"
 
 text = response.text
 soup = BeautifulSoup(text, "html.parser")
-anchor_tags = soup.find_all(name="a", class_="storylink")
 
-for anchor_tag in anchor_tags:
-    print(anchor_tag.string)
+# Getting Title and Link of the posts using anchor tags
+anchor_tags = soup.find_all(name="a", class_="storylink")
+# getting the upvotes of the posts
+up_votes = soup.find_all(name="span", class_="score")
+
+
+i = 0
+while i < len(anchor_tags):
+    print(f"name = {anchor_tags[i].string}")
+    print(f"link = {anchor_tags[i].get("href")}")
+    print(f"up_votes = {up_votes[i].string}")
+    print()
+    i += 1
