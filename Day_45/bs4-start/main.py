fe96acc1ff5cc2beff48f83cd0700927f977984a
Aman88600 <amanbasoya02@gmail.com>
2025-07-18 20:09:28 +0530

    'sorted_list_dict'

--- Day_45/bs4-start/main.py
+++ Day_45/bs4-start/main.py
@@ -15,9 +15,16 @@ up_votes = soup.find_all(name="span", class_="score")
 
 
 i = 0
+name_link_vote = {}
+vote_list = []
 while i < len(anchor_tags):
-    print(f"name = {anchor_tags[i].string}")
-    print(f"link = {anchor_tags[i].get("href")}")
-    print(f"up_votes = {up_votes[i].string}")
-    print()
-    i += 1
+    # print(f"name = {anchor_tags[i].string}")
+    # print(f"link = {anchor_tags[i].get("href")}")
+    vote_list.append(int(up_votes[i].string.split()[0]))
+    # print(f"up_votes = {int(up_votes[i].string.split()[0])}")
+    name_link_vote[int(up_votes[i].string.split()[0])] = [anchor_tags[i].string, anchor_tags[i].get("href")]
+    i += 1  
+
+vote_list.sort(reverse=True)
+for vote in vote_list:
+    print(name_link_vote[vote])
