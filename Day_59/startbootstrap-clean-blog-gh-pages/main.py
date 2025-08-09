249ced2d089e24fa33e0e2bc217bca9d539f873e
Aman88600 <amanbasoya02@gmail.com>
2025-08-09 17:54:56 +0530

    'step_5'

--- Day_59/startbootstrap-clean-blog-gh-pages/main.py
+++ Day_59/startbootstrap-clean-blog-gh-pages/main.py
@@ -6,5 +6,15 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+# Making an about route
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Making a contact route
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
