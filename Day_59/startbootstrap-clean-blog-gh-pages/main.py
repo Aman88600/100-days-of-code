bf11aab5d7f1adf510c4e3330c66c2d6bb00c042
Aman88600 <amanbasoya02@gmail.com>
2025-08-09 19:16:44 +0530

    'day_59'

--- Day_59/startbootstrap-clean-blog-gh-pages/main.py
+++ Day_59/startbootstrap-clean-blog-gh-pages/main.py
@@ -1,11 +1,28 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    # Getting the posts
+    response = requests.get("https://api.npoint.io/e36f46fcb59d92450c79")
+    posts = eval(response.text)
+    return render_template("index.html", posts=posts)
 
+#Post route
+@app.route('/post/<int:id>')
+def get_blog(id):
+    # Getting All Posts
+    posts = requests.get("https://api.npoint.io/e36f46fcb59d92450c79")
+    posts = eval(posts.text)
+
+    # Getting a specific show_post using the id attribute and breaking the loop for optimization
+    for post in posts:
+        if post['id'] == id:
+            show_post = post
+            break
+    return render_template("post.html", post=show_post)
 # Making an about route
 @app.route('/about')
 def about():
